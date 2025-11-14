3faec72ec3600d52da213117ee6d5184887d87be
ChieuVan <nguyenchieuvan13@gmail.com>
2025-11-14 17:15:33 +0700

    create funtion get_current_user with token

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,7 +1,15 @@
 import os
 from passlib.context import CryptContext
-from jose import jwt
+
 from datetime import datetime, timedelta
+
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from jose import jwt,JWTError
+from database import get_db
+import models
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 SECRET_KEY = os.getenv("SECRET_KEY", "secret")
 ALGORITHM = "HS256"
@@ -20,3 +28,18 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+def get_current_user(token: Annotated[str, Depends(oauth2_bearer)], db: Annotated[Session, Depends(get_db)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if not user_id:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        user = db.query(models.User).filter(models.User.id == user_id).first()
+        if not user:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+        return user
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    
