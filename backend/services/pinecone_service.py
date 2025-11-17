08034a1a2c5619a9d76c3f7a69848390b30be326
ChieuVan <nguyenchieuvan13@gmail.com>
2025-11-17 14:27:48 +0700

    creat index vector database PineCone via API

--- backend/services/pinecone_service.py
+++ backend/services/pinecone_service.py
@@ -1,22 +1,34 @@
 from pinecone import Pinecone, ServerlessSpec
-from langchain_pinecone import PineconeVectorStore
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENV = os.getenv("PINECONE_ENV")
 
 pc = Pinecone(api_key=PINECONE_API_KEY)
-# print(pc.list_indexes(), "Pinecone initialized")
-def create_index_if_not_exists(index_name: str, dimension: int):
-    if not pc.has_index(name=index_name):
-        pc.create_index(
-            name=index_name,
-            dimension=dimension,
-            metric="cosine",
-            spec=ServerlessSpec(cloud="aws", region=PINECONE_ENV)
-        )
-    return {"message": f"Index '{index_name}' is ready."}
 
 
+def create_pinecone_index(index_name: str, dimension: int):
+    """Create index if not exists + return status.
+    - if created or exists: return {"status": "ready"}
+    - if error: return {"status": "error", "error": str}
+    """
+    try:
+        # Get list of all indexes
+        existing = pc.list_indexes().names()
+
+        # Create if not exists
+        if index_name not in existing:
+            pc.create_index(
+                name=index_name,
+                dimension=dimension,
+                metric="cosine",
+                spec=ServerlessSpec(cloud="aws", region=PINECONE_ENV),
+            )
+
+        return {"status": "ready"}
+    except Exception as e:
+        print("Error creating Pinecone index:", e)
+        return {"status": "error", "error": str(e)}
